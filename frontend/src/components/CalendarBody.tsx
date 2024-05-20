f263a63a7fc45cc76173099df6724edc5baf64aa
t3yo3shi <f3sss202t@gmail.com>
2024-05-20 20:58:37 +0900

    first commit

---
+++ frontend/src/components/CalendarBody.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+// 初回レンダー用フラグ
+a;
+export default CalendarBody;
---
+++ frontend/src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElem = document.getElementById("root") as HTMLElement;
+const root = createRoot(rootElem);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
