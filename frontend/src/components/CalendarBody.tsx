19959c6ca9215378491873a43067f244cf45e299
t3yo3shi <f3sss202t@gmail.com>
2024-05-22 17:27:30 +0900

    test1

--- frontend/src/components/CalendarBody.tsx
+++ frontend/src/components/CalendarBody.tsx
@@ -1,15 +1,7 @@
 import React from "react";
 
 // 初回レンダー用フラグ
-<<<<<<< HEAD
-<<<<<<< HEAD
-a;
-=======
-
->>>>>>> f1910887ed8a2f37dcb557214431090d13583ee4
-=======
 const CalendarBody = () => {
-  console.log("CalendarBody");
+  console.log("test");
 };
->>>>>>> be402acda0e89e6ce8e19ac8f1216bc50e2e17ba
 export default CalendarBody;
