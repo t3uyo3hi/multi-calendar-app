0841c6e45212d57e4dfb617e5b746b7a03ff62f0
Takeru Iwaki <take10.tennis@gmail.com>
2024-05-26 07:49:09 +0900

    aa

--- frontend/src/components/SidePanel/SidePanel.tsx
+++ frontend/src/components/SidePanel/SidePanel.tsx
@@ -1,10 +1,4 @@
 import React from "react";
 
-const CalendarSidePanel = () => {
-    console.log("CalendarSidePanel");
-};
-console.log("new change");
-console.log("git diff");
-console.log("git diff --staged");
-
+const CalendarSidePanel = () => {};
 export default CalendarSidePanel;
