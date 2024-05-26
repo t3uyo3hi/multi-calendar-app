90a1c9573b57f2cb75e2e090c9ffe5bd484102c6
Takeru Iwaki <take10.tennis@gmail.com>
2024-05-27 08:04:04 +0900

    変更

--- frontend/src/components/CalendarBody.tsx
+++ frontend/src/components/CalendarBody.tsx
@@ -1,17 +1,7 @@
 import React from "react";
-import SidePanel from "./SidePanel/SidePanel";
 
 const CalendarBody = () => {
-    return (
-        <div className="container text-center">
-            <div className="row">
-                <div className="col-8">1 of 2</div>
-                <div className="col-4">
-                    <SidePanel />
-                </div>
-            </div>
-        </div>
-    );
+    return <div className="container text-center">CalendarBody</div>;
 };
 
 // 初回レンダー用フラグ
