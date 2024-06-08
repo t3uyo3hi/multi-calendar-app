aafbfc4dee69e4b3daa22a5eef2d025f256c2751
t3uyo3hi <f3sss202t@gmail.com>
2024-06-08 10:55:16 +0900

    CalendarBaseの統合

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,19 @@ import CalendarHeader from "./components/CalendarHeader";
 import CalendarBody from "./components/CalendarBody";
 import SidePanel from "./components/SidePanel/SidePanel";
 
+import "./css/reset.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./css/style.css";
+
 const App = () => {
-  const [date] = useState(new Date());
+  const [date, setDate] = useState(new Date());
 
   return (
     <div className="container text-center">
       <div className="calendar row">
         <div className="calendar_base shadow">
-          <CalendarHeader />
+          <CalendarHeader date={date} setDate={setDate} />
           <CalendarBody date={date} />
 
           <div className="col-3 l_SidePanel shadow">
