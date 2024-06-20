daf080a1243f8a147730e3e43824c2ff2ba36d8b
t3uyo3hi <f3sss202t@gmail.com>
2024-06-20 21:14:24 +0900

    authディレクトリの削除

--- auth/types/LoginFormInput.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const loginInputSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6),
-});
-
-export type LoginFormInput = z.infer<typeof loginInputSchema>;
--- auth/utils/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import type { Database } from "../types/supabase";
-
-export const supabase = createClient<Database>(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_API_KEY
-);
