1b20fb529e4bcef5642608a16af053a09272c8df
naoyuki <kamaboko912@gmail.com>
2024-06-21 11:54:40 +0900

    webpack.configにてenvを読み取るように修正

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,64 +2,63 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const dotenv = require("dotenv");
 const webpack = require("webpack");
 
-const env = dotenv.config().parsed;
-
-module.exports = {
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env": JSON.stringify(env),
-    }),
-  ],
-  mode: "development",
-  entry: {
-    main: __dirname + "/src/main.tsx",
-  },
-  output: {
-    path: __dirname + "/dist",
-    filename: "[name].js",
-    publicPath: "/",
-  },
-  module: {
-    rules: [
-      {
-        test: [/\.ts$/, /\.tsx$/],
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                "@babel/preset-env",
-                "@babel/preset-react",
-                "@babel/preset-typescript",
-              ],
-            },
-          },
-          {
-            loader: "ts-loader",
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  resolve: {
-    modules: [__dirname + "/node_modules"],
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: __dirname + "/src/index.html",
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: __dirname + "/dist",
+module.exports = () => {
+  const env = dotenv.config().parsed;
+  return {
+    mode: "development",
+    entry: {
+      main: __dirname + "/src/main.tsx",
     },
-    port: 8080,
-    historyApiFallback: true,
-  },
+    output: {
+      path: __dirname + "/dist",
+      filename: "[name].js",
+      publicPath: "/",
+    },
+    module: {
+      rules: [
+        {
+          test: [/\.ts$/, /\.tsx$/],
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                presets: [
+                  "@babel/preset-env",
+                  "@babel/preset-react",
+                  "@babel/preset-typescript",
+                ],
+              },
+            },
+            {
+              loader: "ts-loader",
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    resolve: {
+      modules: [__dirname + "/node_modules"],
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: __dirname + "/src/index.html",
+      }),
+      new webpack.DefinePlugin({
+        "process.env": JSON.stringify(env),
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: __dirname + "/dist",
+      },
+      port: 8080,
+      historyApiFallback: true,
+    },
+  };
 };
