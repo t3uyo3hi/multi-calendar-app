5069d71208b3d1b740446c4b02e1f72f18a562cb
takeyan1999 <take10.tennis@gmail.com>
2024-06-26 20:02:39 +0900

    a

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,64 +15,61 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "./css/style.css";
 
 const App: React.FC = () => {
-  const [date, setDate] = useState(new Date());
-  const [authPage, setAuthPage] = useState("login");
+    const [date, setDate] = useState(new Date());
+    const [authPage, setAuthPage] = useState("login");
 
-  return (
-    <Router>
-      <div className="container text-center">
-        <div className="row">
-          <div className="col-12">
-            <div className="calendar row shadow">
-              <div className="d-flex flex-row calendar_base">
-                <div>
-                  <CalendarHeader date={date} setDate={setDate} />
-                  <DayOfWeekHead />
-                  <CalendarBody date={date} />
+    return (
+        <Router>
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="calendar row shadow">
+                            <div className="d-flex flex-row calendar_base">
+                                <div>
+                                    <CalendarHeader date={date} setDate={setDate} />
+                                    <DayOfWeekHead />
+                                    <CalendarBody date={date} />
+                                </div>
+                                <div className="l_SidePanel col-3">
+                                    <SidePanel />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="l_SidePanel col-3">
-                  <SidePanel />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row mt-5">
-          <div className="col-12">
-            <div className="auth-form p-3 bg-light rounded shadow">
-              <Tab.Container
-                activeKey={authPage}
-                onSelect={(k) => setAuthPage(k!)}
-              >
-                <Nav variant="tabs">
-                  <Nav.Item>
-                    <Nav.Link eventKey="login">Login</Nav.Link>
-                  </Nav.Item>
-                  {/* <Nav.Item>
+                <div className="row mt-5">
+                    <div className="col-12">
+                        <div className="auth-form p-3 bg-light rounded shadow">
+                            <Tab.Container activeKey={authPage} onSelect={(k) => setAuthPage(k!)}>
+                                <Nav variant="tabs">
+                                    <Nav.Item>
+                                        <Nav.Link eventKey="login">Login</Nav.Link>
+                                    </Nav.Item>
+                                    {/* <Nav.Item>
                     <Nav.Link eventKey="reset">Reset Password</Nav.Link>
                   </Nav.Item> */}
-                  <Nav.Item>
-                    <Nav.Link eventKey="signup">Sign Up</Nav.Link>
-                  </Nav.Item>
-                </Nav>
-                <Tab.Content className="mt-3">
-                  <Tab.Pane eventKey="login">
-                    <LoginForm />
-                  </Tab.Pane>
-                  {/* <Tab.Pane eventKey="reset">
+                                    <Nav.Item>
+                                        <Nav.Link eventKey="signup">Sign Up</Nav.Link>
+                                    </Nav.Item>
+                                </Nav>
+                                <Tab.Content className="mt-3">
+                                    <Tab.Pane eventKey="login">
+                                        <LoginForm />
+                                    </Tab.Pane>
+                                    {/* <Tab.Pane eventKey="reset">
                     <ResetPasswordPage />
                   </Tab.Pane> */}
-                  <Tab.Pane eventKey="signup">
-                    <SignUpForm />
-                  </Tab.Pane>
-                </Tab.Content>
-              </Tab.Container>
+                                    <Tab.Pane eventKey="signup">
+                                        <SignUpForm />
+                                    </Tab.Pane>
+                                </Tab.Content>
+                            </Tab.Container>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </Router>
-  );
+        </Router>
+    );
 };
 
 export default App;
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -8,73 +8,65 @@ const env = dotenv.config().parsed;
 
 // 環境変数をオブジェクトに変換
 const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
-  return prev;
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
 }, {});
 
 module.exports = {
-  mode: "development",
-  entry: {
-    main: path.resolve(__dirname, "src/main.tsx"),
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
-    publicPath: "/",
-  },
-  module: {
-    rules: [
-      {
-        test: [/\.ts$/, /\.tsx$/],
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                "@babel/preset-env",
-                "@babel/preset-react",
-                "@babel/preset-typescript",
-              ],
-            },
-          },
-          {
-            loader: "ts-loader",
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  resolve: {
-    modules: [path.resolve(__dirname, "node_modules")],
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/index.html"),
-      templateParameters: {
-        REACT_APP_SUPABASE_URL: env.REACT_APP_SUPABASE_URL,
-        REACT_APP_SUPABASE_API_KEY: env.REACT_APP_SUPABASE_API_KEY,
-      },
-    }),
-    new webpack.DefinePlugin({
-      "process.env.REACT_APP_SUPABASE_URL": JSON.stringify(
-        process.env.REACT_APP_SUPABASE_URL
-      ),
-      "process.env.REACT_APP_SUPABASE_API_KEY": JSON.stringify(
-        process.env.REACT_APP_SUPABASE_API_KEY
-      ),
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "src"),
+    mode: "development",
+    entry: {
+        main: path.resolve(__dirname, "src/main.tsx"),
     },
-    port: 8080,
-    historyApiFallback: true,
-  },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
+        publicPath: "/",
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.ts$/, /\.tsx$/],
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
+                        },
+                    },
+                    {
+                        loader: "ts-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        modules: [path.resolve(__dirname, "node_modules")],
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src/index.html"),
+            templateParameters: {
+                REACT_APP_SUPABASE_URL: env.REACT_APP_SUPABASE_URL,
+                REACT_APP_SUPABASE_API_KEY: env.REACT_APP_SUPABASE_API_KEY,
+            },
+        }),
+        new webpack.DefinePlugin({
+            "process.env.REACT_APP_SUPABASE_URL": JSON.stringify(process.env.REACT_APP_SUPABASE_URL),
+            "process.env.REACT_APP_SUPABASE_API_KEY": JSON.stringify(process.env.REACT_APP_SUPABASE_API_KEY),
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, "src"),
+        },
+        port: 8080,
+        historyApiFallback: true,
+    },
 };
