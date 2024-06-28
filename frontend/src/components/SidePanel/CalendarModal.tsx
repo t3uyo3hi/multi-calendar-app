19d3347d74399cf3dc71f95a848ba8e630b96a06
t3uyo3hi <f3sss202t@gmail.com>
2024-06-29 01:53:04 +0900

    💄 CalendarModalをセンターに表示

--- frontend/src/components/SidePanel/CalendarModal.tsx
+++ frontend/src/components/SidePanel/CalendarModal.tsx
@@ -6,70 +6,72 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 
 const CalendarModal = () => {
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    const addScheduleOption = () => {
-        const options = [];
-        let num = 1;
+  const addScheduleOption = () => {
+    const options = [];
+    let num = 1;
 
-        for (let hour = 0; hour <= 24; hour++) {
-            for (let minute = 0; minute < 60; minute++) {
-                if (minute % 15 == 0) {
-                    const time = `${String(hour).padStart(2, "0")}:${String(minute).padStart(2, "0")}`;
-                    options.push(
-                        <option key={num} value={time}>
-                            {time}
-                        </option>
-                    );
-                    num++;
-                }
-            }
+    for (let hour = 0; hour <= 24; hour++) {
+      for (let minute = 0; minute < 60; minute++) {
+        if (minute % 15 == 0) {
+          const time = `${String(hour).padStart(2, "0")}:${String(
+            minute
+          ).padStart(2, "0")}`;
+          options.push(
+            <option key={num} value={time}>
+              {time}
+            </option>
+          );
+          num++;
         }
-        return options;
-    };
+      }
+    }
+    return options;
+  };
 
-    return (
-        <>
-            <button onClick={handleShow}>
-                <i className="bi bi-plus-circle fs-4"></i>
-            </button>
+  return (
+    <>
+      <button onClick={handleShow}>
+        <i className="bi bi-plus-circle fs-4"></i>
+      </button>
 
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header>
-                    <Modal.Title>予定の追加</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form.Group>
-                        <Form.Control
-                            type="text"
-                            className="fs-5"
-                            placeholder="予定タイトル"
-                            autoComplete="off"
-                            name="title"
-                        />
-                    </Form.Group>
-                    <Form.Group className="mt-3">
-                        <InputGroup>
-                            <Form.Select name="startTime"> {addScheduleOption()}</Form.Select>
-                            <span className="p-1">–</span>
-                            <Form.Select> {addScheduleOption()}</Form.Select>
-                        </InputGroup>
-                    </Form.Group>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        取り消し
-                    </Button>
-                    <Button variant="primary" type="submit" onClick={handleClose}>
-                        追加
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
-    );
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header>
+          <Modal.Title>予定の追加</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group>
+            <Form.Control
+              type="text"
+              className="fs-5"
+              placeholder="予定タイトル"
+              autoComplete="off"
+              name="title"
+            />
+          </Form.Group>
+          <Form.Group className="mt-3">
+            <InputGroup>
+              <Form.Select name="startTime"> {addScheduleOption()}</Form.Select>
+              <span className="p-1">–</span>
+              <Form.Select> {addScheduleOption()}</Form.Select>
+            </InputGroup>
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            取り消し
+          </Button>
+          <Button variant="primary" type="submit" onClick={handleClose}>
+            追加
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
 
 export default CalendarModal;
