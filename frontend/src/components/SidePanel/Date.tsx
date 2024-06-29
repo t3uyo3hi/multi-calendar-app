e64d8d862e0d1b8127b833f9f99ea12d380887c1
t3uyo3hi <f3sss202t@gmail.com>
2024-06-29 10:31:18 +0900

    ⚡️ サイドパネルに今日を反映

--- frontend/src/components/SidePanel/Date.tsx
+++ frontend/src/components/SidePanel/Date.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Weather from "./Weather";
 import WeatherForm from "./WeatherForm";
-import { useState } from "react";
 
-const Date = () => {
-  const [city, setCity] = useState<string>("tokyo");
+type DateProps = {
+  date: Date;
+};
+
+const Date = ({ date }: DateProps) => {
+  const [city, setCity] = useState("tokyo");
+  const dayOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
   return (
     <div>
       <div className="d-flex fs-4 gap-2">
-        <p className="ms-4 mt-3">5/8</p>
+        <p className="ms-4 mt-3">{`${
+          date.getMonth() + 1
+        }/${date.getDate()}`}</p>
         <div className="ms-2">
           <Weather city={city} />
         </div>
       </div>
-      <span>WednesDay</span>
+      <span>{dayOfWeek[date.getDay()]}</span>
       <div className="ms-2">
         <WeatherForm setCity={setCity} />
       </div>
--- frontend/src/components/SidePanel/SidePanel.tsx
+++ frontend/src/components/SidePanel/SidePanel.tsx
@@ -10,57 +10,64 @@ import Event from "./Event";
 import Todo from "./Todo";
 import AccountName from "./AccountName";
 
-const CalendarSidePanel = () => {
-    return (
-        <>
-            <div className="l_SidePanel rounded-end-4 d-flex flex-column justify-content-between position-relative">
-                <div className="SidePanel_header d-flex justify-content-between align-items-center pt-5">
+type SidePanelProps = {
+  date: Date;
+};
+
+const CalendarSidePanel = ({ date }: SidePanelProps) => {
+  return (
+    <>
+      <div className="l_SidePanel rounded-end-4 d-flex flex-column justify-content-between position-relative">
+        <div className="SidePanel_header d-flex justify-content-between align-items-center pt-5">
           <div className="d-flex justify-content-start ms-4 mt-2">
-            <Date />
+            <Date date={date} />
           </div>
-                    <div className="d-flex me-5 mb-4 ">
+          <div className="d-flex me-5 mb-4 ">
             <div>
-                        <LoginModal />
+              <LoginModal />
               <AccountName />
             </div>
           </div>
+        </div>
+
+        <div className="mt-5">
+          <Tab.Container defaultActiveKey="Event">
+            <Tab.Content>
+              <Tab.Pane eventKey="Event">
+                <div className="sidepanel_body_container">
+                  <Event />
                 </div>
+                <CalendarModal />
+              </Tab.Pane>
 
-                <div className="mt-5">
-                    <Tab.Container defaultActiveKey="Event">
-                        <Tab.Content>
-                            <Tab.Pane eventKey="Event">
-                                <div className="sidepanel_body_container">
-                                    <Event />
-                                </div>
-                                <CalendarModal />
-                            </Tab.Pane>
-
-                            <Tab.Pane eventKey="Todo">
-                                <div className="sidepanel_body_container">
-                                    <Todo />
-                                </div>
-                                <TodoModal />
-                            </Tab.Pane>
-                        </Tab.Content>
-
-            
-                        <Nav className="mt-3 justify-content-center gap-5" variant="underline" defaultActiveKey="Event">
-                            <Nav.Item>
-                                <Nav.Link eventKey="Event">
-                                    <i className="fs-3 bi bi-calendar-event"></i>
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="Todo">
-                                    <i className="fs-3 bi bi-check2-square"></i>
-                                </Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                    </Tab.Container>
+              <Tab.Pane eventKey="Todo">
+                <div className="sidepanel_body_container">
+                  <Todo />
                 </div>
-            </div>
-        </>
-    );
+                <TodoModal />
+              </Tab.Pane>
+            </Tab.Content>
+
+            <Nav
+              className="mt-3 justify-content-center gap-5"
+              variant="underline"
+              defaultActiveKey="Event"
+            >
+              <Nav.Item>
+                <Nav.Link eventKey="Event">
+                  <i className="fs-3 bi bi-calendar-event"></i>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="Todo">
+                  <i className="fs-3 bi bi-check2-square"></i>
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Tab.Container>
+        </div>
+      </div>
+    </>
+  );
 };
 export default CalendarSidePanel;
